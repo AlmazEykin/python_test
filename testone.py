47a9db8c5af06d889a1f7bb9afe1c0b92bba18fa
AlmazEykin <Zhora1572@yandex.ru>
2025-05-05 22:02:03 +0300

    sec commit

--- testone.py
+++ testone.py
@@ -1,5 +1,34 @@
-def print_hello_world():
-    print('Hello World!')
-print_hello_world()
+import random
 
-print([1, 2, 4])
+print('Добро пожаловать в числовую угадайку!')
+
+def is_value(s):
+    if s.isdigit() and 1 < int(s) < 101:
+        return True
+    else:
+        return False
+random_num = random.randint(1, 101)
+
+while True:
+    stroka = input('Введите число: ')
+
+    if not is_value(stroka):
+        print('Введите чисЛО в диапазаоне от 1 до 101')
+        continue
+
+
+    if is_value(stroka):
+        stroka = int(stroka)
+
+
+        if stroka == random_num:
+            print('Вы угадали!')
+            break
+
+        elif stroka > random_num:
+            print('Введите число поменьше')
+
+        elif stroka < random_num:
+            print('Введите число побольше')
+
+print('До новых встреч в числовой угадайке!')
